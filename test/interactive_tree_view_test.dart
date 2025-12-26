6e4626132ecb48e295fbfe7362fdc0af433900ac
Yoeri <yoeriz123@gmail.com>
2025-12-26 22:19:57 +0100

    Added tests for #5-on-moved-old-siblings
    Fixed tests to include the edgecase described in #5-on-moved-old-siblings. Also replaced all boolean expect cases to use isTrue and isFalse matchers.

--- test/interactive_tree_view_test.dart
+++ test/interactive_tree_view_test.dart
@@ -39,49 +39,49 @@ void main() {
     test('attach child', () {
       final child = TreeNode('c3', 'child');
 
-      expect(child.isAttached, false);
+      expect(child.isAttached, isFalse);
 
       root2.attachChild(child);
 
-      expect(child.isAttached, true);
+      expect(child.isAttached, isTrue);
       expect(child.parent, root2);
       expect(child.depth, 1);
-      expect(root2.children.contains(child), true);
-      expect(events.contains('attached:c3 to r2'), true);
+      expect(root2.children.contains(child), isTrue);
+      expect(events.contains('attached:c3 to r2'), isTrue);
       expect(controller.getByIdentifier('c3'), child);
     });
 
     test('attach sibling in root', () {
       final newSib = TreeNode('r3', 'root 3');
 
-      expect(newSib.isAttached, false);
+      expect(newSib.isAttached, isFalse);
 
       root1.attachSibling(newSib);
 
-      expect(newSib.isAttached, true);
-      expect(controller.rootNodes.contains(newSib), true);
+      expect(newSib.isAttached, isTrue);
+      expect(controller.rootNodes.contains(newSib), isTrue);
       expect(newSib.parent, null);
       expect(newSib.index, 1);
       expect(newSib.depth, 0);
-      expect(events.contains('attached:r3 to root'), true);
-      expect(events.contains('moved:r2 to 2'), true);
+      expect(events.contains('attached:r3 to root'), isTrue);
+      expect(events.contains('moved:r2 to 2'), isTrue);
       expect(controller.getByIdentifier('r3'), newSib);
     });
 
     test('attach sibling in node', () {
       final newSib = TreeNode('c3', 'child 3');
 
-      expect(newSib.isAttached, false);
+      expect(newSib.isAttached, isFalse);
 
       child1.attachSibling(newSib);
 
-      expect(newSib.isAttached, true);
-      expect(child1.siblings.contains(newSib), true);
+      expect(newSib.isAttached, isTrue);
+      expect(child1.siblings.contains(newSib), isTrue);
       expect(newSib.parent, child1.parent);
       expect(newSib.index, 1);
       expect(newSib.depth, 1);
-      expect(events.contains('attached:c3 to r1'), true);
-      expect(events.contains('moved:c2 to 2'), true);
+      expect(events.contains('attached:c3 to r1'), isTrue);
+      expect(events.contains('moved:c2 to 2'), isTrue);
       expect(controller.getByIdentifier('c3'), newSib);
     });
 
@@ -91,8 +91,8 @@ void main() {
       expect(root1.children.first, child2);
       expect(root1.children.last, child1);
       expect(child1.depth, 1);
-      expect(events.any((e) => e == 'moved:c1 to 1'), true);
-      expect(events.any((e) => e == 'moved:c2 to 0'), true);
+      expect(events.any((e) => e == 'moved:c1 to 1'), isTrue);
+      expect(events.any((e) => e == 'moved:c2 to 0'), isTrue);
     });
 
     test('swap across nested parents', () {
@@ -102,18 +102,18 @@ void main() {
       expect(child3.parent, root1);
       expect(child1.parent, root2);
       expect(child3.depth, 1);
-      expect(root1.children.contains(child3), true);
-      expect(root2.children.contains(child1), true);
-      expect(events.any((e) => e.startsWith('moved:c1')), true);
-      expect(events.any((e) => e.startsWith('moved:c3')), true);
+      expect(root1.children.contains(child3), isTrue);
+      expect(root2.children.contains(child1), isTrue);
+      expect(events.any((e) => e.startsWith('moved:c1')), isTrue);
+      expect(events.any((e) => e.startsWith('moved:c3')), isTrue);
     });
 
     test('move inside same nested parent', () {
       controller.move(child2, 0, newParent: root1);
       expect(root1.children.first, child2);
       expect(child2.depth, 1);
-      expect(events.contains('moved:c2 to 0'), true);
-      expect(events.contains('moved:c1 to 1'), true);
+      expect(events.contains('moved:c2 to 0'), isTrue);
+      expect(events.contains('moved:c1 to 1'), isTrue);
     });
 
     test('move up inside same nested parent with many children', () {
@@ -127,10 +127,10 @@ void main() {
       controller.move(child4, 0, newParent: root1);
       expect(root1.children.first, child4);
       expect(child4.depth, 1);
-      expect(events.contains('moved:c4 to 0'), true);
-      expect(events.contains('moved:c1 to 1'), true);
-      expect(events.contains('moved:c2 to 2'), true);
-      expect(events.contains('moved:c3 to 3'), true);
+      expect(events.contains('moved:c4 to 0'), isTrue);
+      expect(events.contains('moved:c1 to 1'), isTrue);
+      expect(events.contains('moved:c2 to 2'), isTrue);
+      expect(events.contains('moved:c3 to 3'), isTrue);
     });
 
     test('move down inside same nested parent with many children', () {
@@ -144,40 +144,60 @@ void main() {
       controller.move(child1, 4, newParent: root1);
       expect(root1.children.last, child1);
       expect(child4.depth, 1);
-      expect(events.contains('moved:c2 to 0'), true);
-      expect(events.contains('moved:c3 to 1'), true);
-      expect(events.contains('moved:c4 to 2'), true);
-      expect(events.contains('moved:c1 to 3'), true);
+      expect(events.contains('moved:c2 to 0'), isTrue);
+      expect(events.contains('moved:c3 to 1'), isTrue);
+      expect(events.contains('moved:c4 to 2'), isTrue);
+      expect(events.contains('moved:c1 to 3'), isTrue);
     });
 
-    test('move to different nested parent', () {
+    test('move root to parent', () {
       controller.move(root2, 0, newParent: root1);
 
-      expect(controller.rootNodes.contains(root2), false);
-      expect(root1.children.contains(root2), true);
+      expect(controller.rootNodes.contains(root2), isFalse);
+      expect(root1.children.contains(root2), isTrue);
       expect(root2.depth, 1);
-      expect(events.contains('moved:r2 to 0'), true);
-      expect(events.contains('moved:c1 to 1'), true);
-      expect(events.contains('moved:c2 to 2'), true);
+      expect(events.contains('moved:r2 to 0'), isTrue);
+      expect(events.contains('moved:c1 to 1'), isTrue);
+      expect(events.contains('moved:c2 to 2'), isTrue);
+    });
+
+    test('move between parents', () {
+      final child3 = TreeNode('c3', 'child3');
+      final child4 = TreeNode('c4', 'child4');
+
+      root2
+        ..attachChild(child3)
+        ..attachChild(child4);
+
+      controller.move(child3, 0, newParent: root1);
+
+      expect(root1.children.contains(child3), isTrue);
+      expect(root2.children.contains(child3), isFalse);
+      expect(child3.depth, 1);
+
+      expect(events.contains('moved:c1 to 1'), isTrue);
+      expect(events.contains('moved:c2 to 2'), isTrue);
+      expect(events.contains('moved:c3 to 0'), isTrue);
+      expect(events.contains('moved:c4 to 0'), isTrue);
     });
 
     test('remove nested node', () {
       controller.remove(child1);
-      expect(child1.isAttached, false);
-      expect(events.contains('removed:c1 from r1'), true);
-      expect(root1.children.contains(child1), false);
+      expect(child1.isAttached, isFalse);
+      expect(events.contains('removed:c1 from r1'), isTrue);
+      expect(root1.children.contains(child1), isFalse);
     });
 
     test('moveUp/moveDown', () {
       child2.moveUp();
       expect(root1.children.first, child2);
-      expect(events.any((e) => e == 'moved:c2 to 0'), true);
-      expect(events.any((e) => e == 'moved:c1 to 1'), true);
+      expect(events.any((e) => e == 'moved:c2 to 0'), isTrue);
+      expect(events.any((e) => e == 'moved:c1 to 1'), isTrue);
 
       child2.moveDown();
       expect(root1.children.last, child2);
-      expect(events.any((e) => e == 'moved:c2 to 1'), true);
-      expect(events.any((e) => e == 'moved:c1 to 0'), true);
+      expect(events.any((e) => e == 'moved:c2 to 1'), isTrue);
+      expect(events.any((e) => e == 'moved:c1 to 0'), isTrue);
     });
 
     test('Flat List contains all nodes', () {
@@ -196,23 +216,23 @@ void main() {
     test('expand/collapse/toggle on nested node', () {
       child1.expanded = false;
       child1.expand();
-      expect(child1.expanded, true);
+      expect(child1.expanded, isTrue);
 
       child1.collapse();
-      expect(child1.expanded, false);
+      expect(child1.expanded, isFalse);
 
       child1.toggle();
-      expect(child1.expanded, true);
+      expect(child1.expanded, isTrue);
 
       // No callback expected here since expand/collapse/toggle are UI-only
-      expect(events.isEmpty, true);
+      expect(events.isEmpty, isTrue);
     });
     test('traverse on controller hits each node', () {
       final nodes = [];
       controller.traverse((node) => nodes.add(node.identifier));
 
       expect(nodes.length, 4);
-      expect(['r1', 'r2', 'c1', 'c2'].every(nodes.contains), true);
+      expect(['r1', 'r2', 'c1', 'c2'].every(nodes.contains), isTrue);
     });
 
     test('traverse on node hits the node and its descendants', () {
@@ -220,18 +240,18 @@ void main() {
       root1.traverse((node) => nodes.add(node.identifier));
 
       expect(nodes.length, 3);
-      expect(['r1', 'c1', 'c2'].every(nodes.contains), true);
+      expect(['r1', 'c1', 'c2'].every(nodes.contains), isTrue);
     });
 
     test('expand and collapse all', () {
       //by default nodes are all expanded so we start with collapse.
       controller.collapseAll();
 
-      controller.traverse((node) => expect(node.expanded, false));
+      controller.traverse((node) => expect(node.expanded, isFalse));
 
       controller.expandAll();
 
-      controller.traverse((node) => expect(node.expanded, true));
+      controller.traverse((node) => expect(node.expanded, isTrue));
     });
   });
 
